5d7cf9dd066d8d4cfef93312c22917a5e846ad93
gustavomelofr <157622711+gustavomelofr@users.noreply.github.com>
2025-02-11 10:46:11 -0300

    Proprietario.sql

--- src/bancodedados/dados/Proprietario.sql
+++ src/bancodedados/dados/Proprietario.sql
@@ -1,3 +1,4 @@
 INSERT INTO proprietario(email, cpf, senha, nome, telefone) 
     VALUES
     ('joao@unb.br', '777.777.777-77', 'senha', 'João Guilherme', '4002-8922');
+    ('gustavo@unb.br', '666.666.666-66', '1234', 'Gustavo', '4124-9765');
