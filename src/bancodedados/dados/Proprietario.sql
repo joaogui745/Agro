8f5764a1b27feabf7e4a3393b335ae2b941d3366
gustavomelofr <157622711+gustavomelofr@users.noreply.github.com>
2025-02-11 10:57:16 -0300

    Alterando base de dados

--- src/bancodedados/dados/Proprietario.sql
+++ src/bancodedados/dados/Proprietario.sql
@@ -1,6 +1,4 @@
 INSERT INTO proprietario(email, cpf, senha, nome, telefone) 
     VALUES
     ('joao@unb.br', '777.777.777-77', 'senha', 'João Guilherme', '4002-8922');
-    ('gustavo@unb.br', '666.666.666-66', '1234', 'Gustavo', '4124-9765');
-    ('lucca@unb.br', '555.555.555-55', '12345', 'Lucca', '6789-0987');
-    ('caio@unb.br', '999.999.999-99', 'senha', 'Caio', '9865-4567');
+   
